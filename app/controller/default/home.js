78758dcdccecfd7d270da7d4dcd38585f10a49a7
Tim <yjl5062@hotmail.com>
2020-03-16 22:29:31 +0800

    从数据库获取数据

--- app/controller/default/home.js
+++ app/controller/default/home.js
@@ -8,7 +8,7 @@ class HomeController extends Controller {
     ctx.body = 'hi, egg';
   }
   async getArticleList() {
-    const sql = 'select article.id as id,article.title as title,article.introduce as introduce,FROM_UNIXTIME(article.addTime , \'%Y-%m-%d %H:%I:%S\') as addTime, article.artical_content as content,article.view_count as view_count ,type.typeName as typeName from article left join type on article.type_id=type.id';
+    const sql = 'select article.id as id,article.title as title,article.introduce as introduce,article.addTime as addTime,article.view_count as view_count ,type.typeName as typeName from article left join type on article.type_id=type.id';
     const result = await this.app.mysql.query(sql);
     this.ctx.body = {
       data: result,

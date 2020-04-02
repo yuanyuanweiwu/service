34f533ad3c0941a54b57a8af1badf03d408dd433
liuyuanjie <yjl5062@hotmail.com>
2020-04-02 17:18:42 +0800

    修改更改文章接口

--- app/controller/admin/admin.js
+++ app/controller/admin/admin.js
@@ -1,39 +1,52 @@
-'use strict';
+"use strict";
 
-const Controller = require('egg').Controller;
+const Controller = require("egg").Controller;
 
 class AdminController extends Controller {
-  async index() {
-    this.ctx.body = 'ddd';
-  }
   async checkLogin() {
     const userName = this.ctx.request.body.userName;
     const password = this.ctx.request.body.password;
-    const sql = 'select * from admin_user where userName=? and password=?';
-    const res = await this.app.mysql.query(sql, [ userName, password ]);
+    const sql = "select * from admin_user where userName=? and password=?";
+    const res = await this.app.mysql.query(sql, [userName, password]);
     if (res.length > 0) {
       const openId = new Date().getTime();
       this.ctx.session.openId = { openId };
-      this.ctx.body = { data: 'success', openId };
-    
+      this.ctx.body = { data: "success", openId };
     } else {
-      this.ctx.body = { data: 'failed' };
+      this.ctx.body = { data: "failed" };
     }
   }
-  async getTpeInfo(){
-    const resType=await this.app.mysql.select('type')
-    this.ctx.body={data:resType}
+  async getTpeInfo() {
+    const resType = await this.app.mysql.select("type");
+    this.ctx.body = { data: resType };
   }
-  async addArticle(){
-    let tmpArticle=this.ctx.request.body
-    const result= await this.app.mysql.insert('article',tmpArticle)
-    console.log(result)
-    const isSuccess=result.affectedRows===1
-    const insertId=result.insertId
-    this.ctx.body={
+  async addArticle() {
+    let tmpArticle = this.ctx.request.body;
+    const result = await this.app.mysql.insert("article", tmpArticle);
+    console.log(result);
+    const isSuccess = result.affectedRows === 1;
+    const insertId = result.insertId;
+    this.ctx.body = {
       isSuccess,
       insertId
+    };
+  }
+  async updateArticle() {
+    let tmpArticle= this.ctx.request.body
+    const result = await this.app.mysql.update('article', tmpArticle);
+    const updateSuccess = result.affectedRows === 1;
+    console.log(result)
+    this.ctx.body={
+        isSuccess:updateSuccess
     }
+  }
+  async getArticleList() {
+    const result = await this.app.mysql.select("article", {
+      orders: [["id", "desc"]]
+    });
+    this.ctx.body = {
+      list: result
+    };
   }
 }
 
--- app/router/admin.js
+++ app/router/admin.js
@@ -7,4 +7,6 @@ module.exports = app => {
   router.post('/admin/checkLogin', controller.admin.admin.checkLogin);
   router.get('/admin/getTypeInfo',adminauth,controller.admin.admin.getTpeInfo)
   router.post('/admin/addArticle',adminauth,controller.admin.admin.addArticle)
+  router.post('/admin/updateArticle',adminauth,controller.admin.admin.updateArticle)
+  router.get('/admin/getArticleList',adminauth,controller.admin.admin.getArticleList)
 };
